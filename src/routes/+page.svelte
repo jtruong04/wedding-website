c9923a8490349d2928ff69c8ce11f44b50e4d218
John Truong <trucojo@gmail.com>
2024-06-14 22:42:05 -0400

    [ADD] added countdown to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import Hero from '$lib/components/Hero.svelte';
+	import Countdown from '$lib/components/Countdown.svelte';
+	import { DateTime } from 'luxon';
+
+	const date = '10/5/2024, 6:00 PM';
+	const tz = 'America/New_York';
 </script>
 
 <Hero img="photos/banner.jpg">
 	<div class="flex flex-col items-center">
 		<h1 class="script-font text-shadow mb-[5rem] text-6xl font-bold text-white">
+			John and Jessica
+		</h1>
+		<div class="mb-2 text-xl">
+			{DateTime.fromFormat(date, 'f', { zone: tz }).toFormat('EEEE, MMMM dd, t')}
+		</div>
+		<div class="text-white">
+			<Countdown {date} timezone={tz} show_seconds={true} />
+		</div>
 	</div>
 </Hero>
 
